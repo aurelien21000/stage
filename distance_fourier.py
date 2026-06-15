f3abecc4b82d0bca1f59c8bf45668437bf323a04
aurelien21000 <60477249+aurelien21000@users.noreply.github.com>
2026-06-15 13:15:27 +0200

    updating Internship_progress.pdf

--- distance_fourier.py
+++ distance_fourier.py
@@ -13,5 +13,4 @@ def obtenir_spectre_puissance(pos_agents, L=1.0, nb_cases=128):
     spectre = np.abs(fftshift(fourier)) ** 2
 
     return spectre
-
 #then the distance between two patterns is only taking the L2 discrete norm between their spectrum
